b57a9ba1bd05123285bbd82cf85cc045b35d12f6
regnisalram <reka.bence@gmail.com>
2016-12-05 10:29:38 +0100

    Card class modified

--- src/com/greenfox/exams/java/Card.java
+++ src/com/greenfox/exams/java/Card.java
@@ -10,8 +10,10 @@ public class Card {
     String color;
     int value;
 
-    String[] colors = {"treff", "karo", "kor", "pikk"};
-    int[] values = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    Card (String color, int value) {
+        this.color = color;
+        this.value = value;
+    }
 
     public int getValue() {
         return value;
@@ -19,6 +21,6 @@ public class Card {
 
     @Override
     public String toString() {
-        return "color" + " " + "value";
+        return color + " " + value;
     }
 }
